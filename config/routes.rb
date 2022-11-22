9e4ab46232074c304b1037a70afaafc870782365
Vladimir Chernov <v.chernov@zebrains.team>
2022-11-22 13:58:56 +0300

    Changing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root          "static_pages#home"
-  get "help"    => "static_pages#help"
-  get "about"   => "static_pages#about"
-  get "contact" => "static_pages#contact"
+  get "/help"    to: "static_pages#help"
+  get "/about"   to: "static_pages#about"
+  get "/contact" to: "static_pages#contact"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
