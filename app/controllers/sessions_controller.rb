69eb5efd72cfb9afb6f7362ff68b978b28b004b8
Vladimir Ch <dgudo73@mail.ru>
2022-12-09 16:37:29 +0300

    Checking an error in method(the method 'create' don't work)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session[:email].downcase])
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page
     else
       # Create an error message
+      flash[:danger] = "Invalid email/password combination" # Wrong!
       render "new"
     end
   end
